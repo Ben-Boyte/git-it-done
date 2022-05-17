160a371820b3074abf54ad91ed4e8ce96ae05e28
Ben-Boyte <zissette1@gmail.com>
2022-05-17 16:05:00 -0500

    added for loop

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -7,7 +9,7 @@ var getUserRepos = function(user) {
     
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
-        console.log(data);
+        displayRepos(data, user);
       });
     });
 }
@@ -22,6 +24,31 @@ if (username) {
 } else {
   alert("Please enter a GitHub username");
 }
+}
+
+var displayRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+      
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+      
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+      
+        // append to container
+        repoEl.appendChild(titleEl);
+      
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+      }
+
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
