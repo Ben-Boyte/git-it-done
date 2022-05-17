628b6a1ae8997669914448415dd58d0d0f56aa8d
Ben-Boyte <zissette1@gmail.com>
2022-05-17 14:52:21 -0500

    updated js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,27 @@
-var getUserRepos = function(user) {
- var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
- fetch(apiUrl).then(function(response) {
-response.json().then(function(data) {
-    console.log(data);
-    })
- })
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
 }
-getUserRepos();
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
