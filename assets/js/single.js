17114c08314893e2a62397f7ac96beb12364ffc4
Ben-Boyte <zissette1@gmail.com>
2022-05-18 14:41:02 -0500

    added more js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,46 +1,77 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
-            })
+  
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
-        else {
-            alert("There was a problem with your request!");
-        }
-    })
-}
+      });
+    }
+    else {
+      console.log(response);
+      alert("There was a problem with your request!");
+    }
+  });
+};
 
 var displayIssues = function(issues) {
-    if (issues.length === 0) {
-        issueContainerEl.textContent = "This repo has no open issues!";
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  
+  for (var i = 0; i < issues.length; i++) {
+  
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    
+  
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+    
+  
+  
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement("span");
+
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    }
+    else {
+      typeEl.textContent = "(Issue)";
     }
 
-    for (var i = 0; i < issues.length; i++) {
-        var issueEl = document.createElement("a");
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blank");
+    issueEl.appendChild(typeEl);
+  
+  
+    issueContainerEl.appendChild(issueEl);
+  }
+};
 
-        var titleEl = document.createElement("span");
-        titleEl.textContent = issues[i].title;
+var displayWarning = function(repo) {
+  
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-        issueEl.appendChild(titleEl);
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
 
-        var typeEl = document.createElement("span");
-
-        if (issues[i].pull_request) {
-            typeEl.textContent = "(Pull Request)";
-        } else {
-            typeEl.textContent = "(Issue)";
-        }
-        issueEl.appendChild(typeEl);
-
-        issueContainerEl.appendChild(issueEl);
-    }
-}
+  limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
